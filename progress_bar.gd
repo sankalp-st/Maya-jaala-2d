a83a97471794ff6d94824936d05cb7354a3f3931
sankalp-st <163658679+sankalp-st@users.noreply.github.com>
2025-12-01 21:40:54 +0530

    Add files via upload

---
+++ progress_bar.gd
@@ -0,0 +1,22 @@
+extends ProgressBar
+
+
+var parent
+var max_value_amount
+var min_value_amount
+
+
+func _ready() -> void:
+	parent = get_parent().get_parent()
+	max_value_amount = parent.health_max
+	min_value_amount = parent.health_min
+
+	
+func _process(delta: float) -> void:
+	self.value = parent.health 
+	if parent.health != max_value_amount :
+		self.visible = true
+		if parent.health == min_value_amount:
+			self.visible = false
+	else:
+		self.visible = true
